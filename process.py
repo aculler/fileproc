7215f06abcceb490c4c639dcd0de016150af6959
Andy <adc4392@gmail.com>
2017-11-27 22:22:57 -0600

    Work in progress converting to just use jobid.
    I realized that the whole purpose of having references to the file in the jobs table was
    so that you could just have the job_id you want to process and do everything. In the middle
    of refactoring to make that work as well as updating the status of the job as it's in progress.
    
    I also want to abstract the session management stuff since each of these functions is going
    to have a very similar session lifecycle. Also, I need to reference some of the sqlalchemy
    session management documentation, since they have some opinions about session lifecycles.

--- process.py
+++ process.py
@@ -4,28 +4,41 @@ from hashlib import md5
 import magic
 
 from database import Session
-from database.models import File
+from database.models import File, Job
 
 
-def parse_metadata(fileid, jobid):
+# TODO: It would be cool to write a decorator to do the job status transitioning. Would need to make sure it could
+# properly account for the error handling.
+def parse_metadata(jobid):
     session = Session()
-    file_obj = session.query(File).filter(File.id == fileid).first()
-    match = re.search(
-        r'(.+)\.([a-z0-9]+)$',
-        file_obj.abs_path.split('/')[-1],
-        re.I
-    )
-    filename = match.group(1)
-    extension = match.group(2)
-
-    file_type = magic.from_file(file_obj.abs_path)
-    mime_type = magic.from_file(file_obj.abs_path, mime=True)
-
-    file_obj.name = filename
-    file_obj.ext = extension
-    file_obj.file_type = file_type
-    file_obj.mime_type = mime_type
+    file_obj, job = session.query(File, Job).filter(Job.id == jobid).first()
+    job.status = 'working'
     session.commit()
+    try:
+        match = re.search(
+            r'(.+)\.([a-z0-9]+)$',
+            file_obj.abs_path.split('/')[-1],
+            re.I
+        )
+        filename = match.group(1)
+        extension = match.group(2)
+
+        file_type = magic.from_file(file_obj.abs_path)
+        mime_type = magic.from_file(file_obj.abs_path, mime=True)
+
+        file_obj.name = filename
+        file_obj.ext = extension
+        file_obj.file_type = file_type
+        file_obj.mime_type = mime_type
+        # TODO: Need to figure out why this status isn't getting updated even though the file_obj is and the job stauts
+        # is properly updating to 'working' above.
+        job.staus = 'complete'
+        print('session.dirty: {0}'.format(session.dirty))
+        session.commit()
+    except:
+        session.rollback()
+        job.status = 'failed'
+        session.commit()
 
 def hash_file(file_obj, job):
     md5sum = md5(open(file_obj.abs_path, 'rb').read()).hexdigest()
